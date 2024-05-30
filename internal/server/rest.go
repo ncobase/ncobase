94f87b1889e7d47dffbd30ec5e535837a42c63ac
Shen <ihaiyon@gmail.com>
2024-05-30 10:23:20 +0800

    health check & root url handler

--- internal/server/rest.go
+++ internal/server/rest.go
@@ -1,8 +1,11 @@
 package server
 
 import (
+	"net/http"
+	"stocms/internal/config"
 	"stocms/internal/handler"
 	"stocms/internal/server/middleware"
+	"stocms/pkg/util"
 
 	"github.com/swaggo/files"
 	"github.com/swaggo/gin-swagger"
@@ -10,9 +13,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func registerRestRouter(e *gin.Engine, h *handler.Handler) {
-	// Handler prefix
-	v1 := e.Group("/")
+func registerRest(e *gin.Engine, h *handler.Handler, conf *config.Config) {
+	// root Jump when domain is configured and it is not localhost
+	e.GET("/", func(ctx *gin.Context) {
+		if conf.Domain != "localhost" {
+			url := util.GetDomain(conf, conf.Domain)
+			ctx.Redirect(http.StatusMovedPermanently, url)
+		} else {
+			ctx.String(http.StatusOK, "It's working.")
+		}
+	})
+
+	// Health
+	e.GET("/health", h.Ping)
+
+	// api prefix for v1 version
+	v1 := e.Group("/v1")
+
 	// Authorize
 	v1.GET("/authorize", middleware.Authorized, nil)
 	// Hello
