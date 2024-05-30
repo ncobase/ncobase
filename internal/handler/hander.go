d5731792543021434e8baa0e2a699eb6ea14c9b4
Shen <ihaiyon@gmail.com>
2024-05-30 10:23:20 +0800

    health check & root url handler

--- internal/handler/hander.go
+++ internal/handler/hander.go
@@ -1,7 +1,12 @@
 package handler
 
 import (
+	"context"
 	"stocms/internal/service"
+	"stocms/pkg/log"
+	"stocms/pkg/resp"
+
+	"github.com/gin-gonic/gin"
 )
 
 // Handler represents a handler definition.
@@ -12,4 +17,12 @@ type Handler struct {
 // New creates a new Handler instance.
 func New(svc *service.Service) *Handler {
 	return &Handler{svc}
+}
+
+// Ping health status
+func (h *Handler) Ping(ctx *gin.Context) {
+	if err := h.svc.Ping(ctx); err != nil {
+		log.Fatalf(context.Background(), "ping error: %+v", err)
+	}
+	resp.Success(ctx, nil)
 }
