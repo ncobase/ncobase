85fd06f2842c104b4fc5089cf5f0c8556ceecfcb
Shen <ihaiyon@gmail.com>
2024-06-03 16:11:26 +0800

    improved middlewares and log storage * format code * improved logger * add trace

--- internal/helper/context.go
+++ internal/helper/context.go
@@ -3,6 +3,7 @@ package helper
 import (
 	"context"
 	"stocms/internal/config"
+	"stocms/pkg/nanoid"
 
 	"github.com/gin-gonic/gin"
 )
@@ -107,4 +108,40 @@ func GetProfile(c *gin.Context) any {
 // SetProfile sets profile to gin.Context
 func SetProfile(c *gin.Context, profile any) {
 	SetValue(c, "profile", profile)
+}
+
+// GetRequestID gets request id from gin.Context
+func GetRequestID(c *gin.Context) string {
+	if rid, ok := GetValue(c, "request_id").(string); ok {
+		return rid
+	}
+	return ""
+}
+
+// SetRequestID sets request id to gin.Context
+func SetRequestID(c *gin.Context, rid string) {
+	SetValue(c, "request_id", rid)
+}
+
+// GetTraceID gets trace id from gin.Context
+func GetTraceID(c *gin.Context) string {
+	if traceID, ok := GetValue(c, "trace_id").(string); ok {
+		return traceID
+	}
+	return ""
+}
+
+// SetTraceID sets trace id to gin.Context
+func SetTraceID(c *gin.Context, traceID string) {
+	SetValue(c, "trace_id", traceID)
+}
+
+// NewTraceID creates a new trace ID.
+func NewTraceID() string {
+	return nanoid.Must(16)
+}
+
+// NewRequestID creates a new request ID.
+func NewRequestID() string {
+	return nanoid.Must(16)
 }
---
+++ internal/server/middleware/trace.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"stocms/internal/helper"
+	"stocms/pkg/log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Trace(c *gin.Context) {
+	// Get the trace ID from the request
+	traceID := helper.GetTraceID(c)
+
+	// If trace ID is not present in the request, generate a new one
+	if traceID == "" {
+		traceID = helper.NewTraceID()
+		// Set the trace ID in the request context
+		ctx := log.NewTraceIDContext(c.Request.Context(), traceID)
+		c.Request = c.Request.WithContext(ctx)
+	}
+
+	// Set trace header in the response
+	c.Writer.Header().Set("X-Trace-ID", traceID)
+
+	c.Next()
+}
