04f1c15edaca31eaf70b4ef8805e5d3aa7a3a12b
Shen <ihaiyon@gmail.com>
2024-06-13 10:16:00 +0800

    response exception add errors * rename error to status

--- pkg/resp/status.go
+++ pkg/resp/status.go
@@ -3,74 +3,73 @@ package resp
 import (
 	"net/http"
 	"stocms/pkg/ecode"
-	"stocms/pkg/types"
 )
 
-// newResponse creates a new response.
-func newResponse(status int, code int, message string, data ...types.JSON) *Exception {
-	var responseData any
-	if len(data) > 0 {
-		responseData = data[0]
-	}
-	return &Exception{
-		Status:  status,
-		Code:    code,
-		Message: message,
-		Data:    responseData,
-	}
-}
-
 // AlreadyExists indicates that the resource already exists.
-func AlreadyExists(message string, data ...types.JSON) *Exception {
-	return newResponse(http.StatusOK, 2003, message, data...)
+func AlreadyExists(message string, data ...any) *Exception {
+	return newResponse(http.StatusConflict, ecode.Conflict, message, data...)
 }
 
 // NotExists indicates that the resource does not exist.
-func NotExists(message string, data ...types.JSON) *Exception {
-	return newResponse(http.StatusOK, 2002, message, data...)
+func NotExists(message string, data ...any) *Exception {
+	return newResponse(http.StatusNotFound, ecode.NothingFound, message, data...)
 }
 
 // DBQuery indicates a database query error.
-func DBQuery(message string, data ...types.JSON) *Exception {
+func DBQuery(message string, data ...any) *Exception {
 	return newResponse(http.StatusInternalServerError, ecode.ServerErr, message, data...)
 }
 
 // Transactions indicates a transaction processing failure.
-func Transactions(message string, data ...types.JSON) *Exception {
+func Transactions(message string, data ...any) *Exception {
 	return newResponse(http.StatusInternalServerError, ecode.ServerErr, message, data...)
 }
 
 // UnAuthorized indicates that the request is unauthorized.
-func UnAuthorized(message string, data ...types.JSON) *Exception {
+func UnAuthorized(message string, data ...any) *Exception {
 	return newResponse(http.StatusUnauthorized, ecode.Unauthorized, message, data...)
 }
 
 // BadRequest indicates a bad request.
-func BadRequest(message string, data ...types.JSON) *Exception {
+func BadRequest(message string, data ...any) *Exception {
 	return newResponse(http.StatusBadRequest, ecode.RequestErr, message, data...)
 }
 
 // NotFound indicates that the requested resource is not found.
-func NotFound(message string, data ...types.JSON) *Exception {
+func NotFound(message string, data ...any) *Exception {
 	return newResponse(http.StatusNotFound, ecode.NothingFound, message, data...)
 }
 
 // Forbidden indicates access is forbidden.
-func Forbidden(message string, data ...types.JSON) *Exception {
+func Forbidden(message string, data ...any) *Exception {
 	return newResponse(http.StatusForbidden, ecode.AccessDenied, message, data...)
 }
 
 // InternalServer indicates a server error.
-func InternalServer(message string, data ...types.JSON) *Exception {
+func InternalServer(message string, data ...any) *Exception {
 	return newResponse(http.StatusInternalServerError, ecode.ServerErr, message, data...)
 }
 
 // Conflict indicates a conflict error.
-func Conflict(message string, data ...types.JSON) *Exception {
+func Conflict(message string, data ...any) *Exception {
 	return newResponse(http.StatusConflict, ecode.Conflict, message, data...)
 }
 
 // NotAllowed indicates a not allowed error.
-func NotAllowed(message string, data ...types.JSON) *Exception {
+func NotAllowed(message string, data ...any) *Exception {
 	return newResponse(http.StatusMethodNotAllowed, ecode.MethodNotAllowed, message, data...)
+}
+
+// newResponse creates a new response.
+func newResponse(status, code int, message string, data ...any) *Exception {
+	var responseData any
+	if len(data) > 0 {
+		responseData = data[0]
+	}
+
+	if status < 200 || status >= 400 || code == 0 {
+		return response(code, message, nil, responseData)
+	}
+
+	return response(code, message, responseData, nil)
 }
