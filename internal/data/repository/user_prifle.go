cce9455e2adb5da12fe30e4a4b39935f8e4b677c
Shen <ihaiyon@gmail.com>
2024-06-22 13:35:29 +0800

    user profile add cache

--- internal/data/repository/user_prifle.go
+++ internal/data/repository/user_prifle.go
@@ -2,6 +2,8 @@ package repo
 
 import (
 	"context"
+	"fmt"
+	"ncobase/common/types"
 	"ncobase/internal/data"
 	"ncobase/internal/data/ent"
 	userProfileEnt "ncobase/internal/data/ent/userprofile"
@@ -16,6 +18,7 @@ import (
 // UserProfile represents the user profile repository interface.
 type UserProfile interface {
 	Create(ctx context.Context, body *structs.UserProfileBody) (*ent.UserProfile, error)
+	Update(ctx context.Context, id string, updates types.JSON) (*ent.UserProfile, error)
 	Get(ctx context.Context, id string) (*ent.UserProfile, error)
 	Delete(ctx context.Context, id string) error
 }
@@ -43,6 +46,16 @@ func (r *userProfileRepo) Create(ctx context.Context, body *structs.UserProfileB
 	builder.SetID(body.ID)
 	builder.SetDisplayName(body.DisplayName)
 	builder.SetShortBio(body.ShortBio)
+	builder.SetNillableAbout(body.About)
+	builder.SetNillableThumbnail(body.Thumbnail)
+
+	if body.Links != nil {
+		builder.SetLinks(types.ToValue(body.Links))
+	}
+
+	if body.Extras != nil {
+		builder.SetExtras(types.ToValue(body.Extras))
+	}
 
 	// execute the builder.
 	row, err := builder.Save(ctx)
@@ -51,11 +64,56 @@ func (r *userProfileRepo) Create(ctx context.Context, body *structs.UserProfileB
 		return nil, err
 	}
 
+	// cache the result
+	err = r.c.Set(ctx, fmt.Sprintf("user_profile=%s", body.ID), row)
+	if err != nil {
+		log.Errorf(context.Background(), "userProfileRepo.CreateProfile cache error: %v\n", err)
+	}
+
+	return row, nil
+}
+
+// Update update user profile
+func (r *userProfileRepo) Update(ctx context.Context, id string, updates types.JSON) (*ent.UserProfile, error) {
+
+	// create builder
+	builder := r.ec.UserProfile.UpdateOneID(id)
+
+	for field, value := range updates {
+		switch field {
+		case "display_name":
+			builder.SetNillableDisplayName(types.ToPointer(value.(string)))
+		case "short_bio":
+			builder.SetNillableShortBio(types.ToPointer(value.(string)))
+		case "about":
+			builder.SetNillableAbout(types.ToPointer(value.(string)))
+		case "thumbnail":
+			builder.SetNillableThumbnail(types.ToPointer(value.(string)))
+		case "links":
+			builder.SetLinks(value.([]types.JSON))
+		case "extras":
+			builder.SetExtras(value.(types.JSON))
+		}
+	}
+
+	// execute the builder
+	row, err := builder.Save(ctx)
+	if err != nil {
+		log.Errorf(context.Background(), "userProfileRepo.UpdateProfile error: %v\n", err)
+		return nil, err
+	}
+
 	return row, nil
 }
 
 // Get find profile by user id
 func (r *userProfileRepo) Get(ctx context.Context, id string) (*ent.UserProfile, error) {
+	// try get from cache
+	cacheKey := fmt.Sprintf("user_profile=%s", id)
+	if cached, err := r.c.Get(ctx, cacheKey); err == nil && cached != nil {
+		return cached, nil
+	}
+
 	row, err := r.ec.UserProfile.
 		Query().
 		Where(userProfileEnt.IDEQ(id)).
@@ -65,6 +123,13 @@ func (r *userProfileRepo) Get(ctx context.Context, id string) (*ent.UserProfile,
 		log.Errorf(context.Background(), "userProfileRepo.GetProfile error: %v\n", err)
 		return nil, err
 	}
+
+	// cache the result
+	err = r.c.Set(ctx, cacheKey, row)
+	if err != nil {
+		log.Errorf(context.Background(), "userProfileRepo.GetProfile cache error: %v\n", err)
+	}
+
 	return row, nil
 }
 
@@ -74,5 +139,13 @@ func (r *userProfileRepo) Delete(ctx context.Context, id string) error {
 		log.Errorf(context.Background(), "userProfileRepo.Delete error: %v\n", err)
 		return err
 	}
+
+	// remove from cache
+	cacheKey := fmt.Sprintf("user_profile=%s", id)
+	err := r.c.Delete(ctx, cacheKey)
+	if err != nil {
+		log.Errorf(context.Background(), "userProfileRepo.Delete cache error: %v\n", err)
+	}
+
 	return nil
 }
