ad75aefb96cede04e84a7420fff8149212af81c6
Shen <ihaiyon@gmail.com>
2024-09-02 09:27:59 +0800

    validate request time difference

---
+++ cmd/ncobase/middleware/timestamp.go
@@ -0,0 +1,47 @@
+package middleware
+
+import (
+	"ncobase/common/resp"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Timestamp middleware for checking timestamp
+func Timestamp(c *gin.Context) {
+	_t := c.Query("_t")
+	if _t == "" {
+		resp.Fail(c.Writer, resp.BadRequest("Missing timestamp parameter '_t'"))
+		c.Abort()
+		return
+	}
+
+	timestamp, err := strconv.ParseInt(_t, 10, 64)
+	if err != nil {
+		resp.Fail(c.Writer, resp.BadRequest("Invalid timestamp format"))
+		c.Abort()
+		return
+	}
+
+	now := time.Now().UnixMilli()
+
+	diff := abs(now - timestamp)
+
+	// 15 minutes
+	if diff > 900000 {
+		resp.Fail(c.Writer, resp.BadRequest("Timestamp is more than 15 minutes off"))
+		c.Abort()
+		return
+	}
+
+	c.Next()
+}
+
+// abs returns the absolute value of x.
+func abs(n int64) int64 {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
